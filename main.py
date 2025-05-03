dd836d648a6720619e4c5466030ea4a61a9251d8
kishantiwarii <118036092+kishantiwarii@users.noreply.github.com>
2025-05-03 10:54:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import eel
 from engine.feature import *
 from engine.command import *
 
-eel.init("www")
-playAssistantSound()
+def start():
 
-os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+ eel.init("www")
+ playAssistantSound()
 
-eel.start('index.html', mode=None, host='localhost', block=True)
+ os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
+ eel.start('index.html', mode=None, host='localhost', block=True)
